cc71ddf797820028249a9cc4ea8c87f05cb888d2
Matheus Messora <matheus.messora.vpn@gmail.com>
2016-08-29 23:09:24 -0300

    JS data inclusion
    Included Handlebars framework.
    Included Jquery framework.
    
    Create pokemon.js for data

---
+++ pokemon.js
@@ -0,0 +1,18 @@
+var pokemons = [{
+		"id" : 1,
+		"name" : "Bulbasaur",
+		"type" : ["grass", "poison"]
+	}, {
+		"id" : 2,
+		"name" : "Ivysaur",
+		"type" : ["grass", "poison"]
+	}, {
+		"id" : 3,
+		"name" : "Venusaur",
+		"type" : ["grass", "poison"]
+	}, {
+		"id" : 4,
+		"name" : "Charmander",
+		"type" : ["fire"]
+	}
+]
